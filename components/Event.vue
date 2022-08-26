f70c70778aae49ca844de5a7b6c7c039106a48bc
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2022-08-26 23:58:19 +0200

    Fix alignment

--- components/Event.vue
+++ components/Event.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="w-full rounded bg-gray-900 h-52 sm:mr-3 p-5">
+  <div class="w-full rounded bg-gray-900 h-64 mb-3 sm:mr-3 p-5">
     <h3 class="leading-loose mb-2 text-green-500 font-bold">
       <span class="text-2xl">{{ event.name }}</span>
-      <span class="text-1xl text-gray-200 font-normal">| {{ event.date }}</span>
+      <span class="text-1xl text-gray-200 font-normal">
+        | {{ event.date }}</span
+      >
     </h3>
     <h4 class="leading-loose text-2xl mb-8">
       {{ event.title }}
@@ -21,8 +23,5 @@
 export default {
   name: "Event",
   props: ["event"],
-  setup(props) {
-    console.log(props.event.title);
-  },
 };
 </script>
