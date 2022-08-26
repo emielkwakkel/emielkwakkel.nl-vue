65efd445639cc7f9f43b09e20fa6568f6fe9984f
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2022-08-26 22:57:15 +0200

    Initial commit

---
+++ components/Event.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="w-full rounded bg-gray-900 h-52 sm:mr-3 p-5">
+    <h3 class="leading-loose mb-2 text-green-500 font-bold">
+      <span class="text-2xl">{{ event.name }}</span>
+      <span class="text-1xl text-gray-200 font-normal">| {{ event.date }}</span>
+    </h3>
+    <h4 class="leading-loose text-2xl mb-8">
+      {{ event.title }}
+    </h4>
+    <a
+      v-if="event.link"
+      :href="event.link.href"
+      class="bg-blue-300 text-gray-800 font-bold rounded-full mt-6 py-4 px-8 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+    >
+      {{ event.link.title }}
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Event",
+  props: ["event"],
+  setup(props) {
+    console.log(props.event.title);
+  },
+};
+</script>
