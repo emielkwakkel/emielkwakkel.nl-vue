4787de3c31a60fe5e710d43fbc72a1b9af1d9e9b
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2022-09-09 23:26:39 +0200

    Ondersteuning voor Nuxt content toevoegen voor blogging

---
+++ content/blog/sustainable-choices.md
@@ -0,0 +1,19 @@
+---
+title: "Title of the page"
+description: "meta description of the page"
+draft: true
+head:
+  meta:
+    - name: "keywords"
+      content: "sustainability"
+    - name: "robots"
+      content: "index, follow"
+    - name: "author"
+      content: "Emiel Kwakkel"
+    - name: "copyright"
+      content: "© 2022 Emiel Kwakkel"
+---
+
+# Sustainable Frontend Development
+
+Content
---
+++ pages/[...slug].vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-gray-900">
+    <div>
+      <div class="flex container h-full px-3 bg-gray-900 text-gray-200 mx-auto">
+        <div class="flex flex-col w-full">
+          <ContentDoc>
+            <template #not-found>
+              <h1>Document not found</h1>
+            </template>
+          </ContentDoc>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
