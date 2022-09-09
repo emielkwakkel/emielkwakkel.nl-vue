718ef54ab43db6e79a5ffed6d36b5a36a4b0af1f
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2022-09-09 23:55:12 +0200

    Render title at blog detail page

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -3,10 +3,9 @@
     <div>
       <div class="flex container h-full px-3 bg-gray-900 text-gray-200 mx-auto">
         <div class="flex flex-col w-full">
-          <ContentDoc>
-            <template #not-found>
-              <h1>Document not found</h1>
-            </template>
+          <ContentDoc v-slot="{ doc }">
+            <h1>{{ doc.title }}</h1>
+            <ContentRenderer :value="doc" />
           </ContentDoc>
         </div>
       </div>
