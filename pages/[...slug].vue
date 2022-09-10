1aeb2350385317c6d6f112e1fdc138a1ed1e32ab
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2022-09-10 14:09:29 +0200

    Style title of blog

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -4,7 +4,9 @@
       <div class="flex container h-full px-3 bg-gray-900 text-gray-200 mx-auto">
         <div class="flex flex-col w-full">
           <ContentDoc v-slot="{ doc }">
-            <h1>{{ doc.title }}</h1>
+            <h1 class="leading-normal mb-2 text-green-500 text-5xl font-bold">
+              {{ doc.title }}
+            </h1>
             <ContentRenderer :value="doc" />
           </ContentDoc>
         </div>
