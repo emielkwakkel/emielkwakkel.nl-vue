7efe38d35fff5deb7176185415f9a97f56418be7
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2022-09-10 17:05:32 +0200

    Show alt tag as caption

---
+++ components/content/ProseImg.vue
@@ -0,0 +1,34 @@
+<template>
+  <img :src="src" :alt="alt" :width="width" :height="height" />
+  <p v-if="alt">
+    <em>{{ alt }}</em>
+  </p>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  src: {
+    type: String,
+    default: "",
+  },
+  alt: {
+    type: String,
+    default: "",
+  },
+  width: {
+    type: [String, Number],
+    default: undefined,
+  },
+  height: {
+    type: [String, Number],
+    default: undefined,
+  },
+});
+</script>
+
+<style scoped>
+p {
+  line-height: 1.8em;
+  font-size: 1.2em;
+}
+</style>
