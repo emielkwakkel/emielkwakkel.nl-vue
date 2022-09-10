028ebc8de904e8d0bd62aa0c134f3d44963c275e
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2022-09-11 00:26:25 +0200

    Add navigation to blogitems

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,12 +1,24 @@
 <template>
   <div class="bg-gray-900">
+    <nav class="bg-blue-300">
+      <p class="flex container mx-auto px-2">
+        <a
+          href="/"
+          class="bg-gray-900 text-white font-bold rounded-full my-6 py-4 px-8 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+        >
+          &lt; Home
+        </a>
+      </p>
+    </nav>
     <section
-      class="flex container h-full px-3 bg-gray-900 text-gray-200 mx-auto"
+      class="flex container h-full px-3 bg-gray-900 text-gray-200 mx-auto mb-20"
     >
       <article class="flex flex-col w-full">
         <ContentDoc v-slot="{ doc }">
           <header>
-            <h1 class="leading-normal mb-2 text-green-500 text-5xl font-bold">
+            <h1
+              class="leading-normal mb-2 mt-12 text-green-500 text-5xl font-bold"
+            >
               {{ doc.title }}
             </h1>
           </header>
@@ -14,5 +26,15 @@
         </ContentDoc>
       </article>
     </section>
+    <nav class="bg-blue-300">
+      <p class="flex container mx-auto px-2">
+        <a
+          href="/"
+          class="bg-gray-900 text-white font-bold rounded-full my-6 py-4 px-8 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+        >
+          &lt; Home
+        </a>
+      </p>
+    </nav>
   </div>
 </template>
