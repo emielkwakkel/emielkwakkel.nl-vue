4cbc92c658e261ed69ee4eb6fea731acedfbb409
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2024-12-09 21:41:22 +0100

    Add links to blogs in blog overview and fix light theme issues

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="dark:bg-gray-900 bg-white">
+  <div
+    class="min-h-screen dark:bg-gray-900 dark:text-gray-200 text-gray-900 bg-white"
+  >
     <Breadcrumb :links="links"></Breadcrumb>
     <div>
       <header
@@ -9,28 +11,33 @@
           class="flex flex-col w-full justify-center text-center md:text-left"
         >
           <h1 class="leading-normal mb-2 text-green-500 text-5xl font-bold">
-            Blog <span class="text-white">for a</span> greener world
+            Blog
+            <span class="dark:text-white text-gray-900">for a</span> greener
+            world
           </h1>
         </div>
       </header>
 
-      <section class="w-full pt-20 pb-20 bg-gray-950">
-        <div
-          class="container mx-auto sm:flex w-full px-3 text-gray-200 p-3 pb-10"
-        >
-          <ContentList :path="`/${locale}/blog`" v-slot="{ list }">
-            <article
+      <section class="w-full pt-20 pb-20 dark:bg-gray-950 bg-gray-200">
+        <ContentList :path="`/${locale}/blog`" v-slot="{ list }">
+          <div class="container mx-auto sm:flex w-full px-3 p-3 pb-10">
+            <router-link
               v-for="article in list"
               :key="article._path"
+              :to="article._path!"
               class="w-full rounded dark:bg-gray-900 bg-white h-64 mb-3 sm:mr-3 p-5"
             >
-              <h2 class="leading-normal mb-2 text-green-500 text-3xl font-bold">
-                {{ article.title }}
-              </h2>
-              <p>{{ article.description }}</p>
-            </article>
-          </ContentList>
-        </div>
+              <article>
+                <h2
+                  class="leading-normal mb-2 text-green-500 text-3xl font-bold"
+                >
+                  {{ article.title }}
+                </h2>
+                <p>{{ article.description }}</p>
+              </article>
+            </router-link>
+          </div>
+        </ContentList>
       </section>
     </div>
   </div>
