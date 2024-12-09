e3d9509540a3f02baaf282dd52f795c97e564e0a
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2024-12-09 21:41:55 +0100

    Implement blog overview component

---
+++ components/Blog.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    :key="article._path"
+    class="w-full rounded dark:bg-gray-900 bg-gray-200 dark:text-white text-gray-900 h-64 mb-3 sm:mr-3 p-5"
+  >
+    <h2 class="leading-normal mb-2 text-green-500 text-3xl font-bold">
+      {{ article.title }}
+    </h2>
+    <p>{{ article.description }}</p>
+    <p class="mt-6">
+      <a
+        :href="article._path"
+        class="dark:bg-gray-950 bg-white font-bold rounded-full py-4 px-8 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+      >
+        {{ $t("generic.readmore") }}
+      </a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Blog",
+  props: ["article"],
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -52,28 +52,11 @@
     <Section>
       <template v-slot:header>{{ $t("pages.home.latest-blogs") }}</template>
       <ContentList :path="`/${locale}/blog`" v-slot="{ list }">
-        <div
-          v-for="article in list"
-          :key="article._path"
-          class="w-full rounded dark:bg-gray-950 bg-gray-200 h-64 mb-3 sm:mr-3 p-5"
-        >
-          <h2 class="leading-normal mb-2 text-green-500 text-3xl font-bold">
-            {{ article.title }}
-          </h2>
-          <p>{{ article.description }}</p>
-          <p class="mt-6">
-            <a
-              :href="article._path"
-              class="dark:bg-gray-900 bg-white font-bold rounded-full mt-12 py-4 px-8 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
-            >
-              {{ $t("generic.readmore") }}
-            </a>
-          </p>
-        </div>
+        <Blog v-for="article in list" :key="article.title" :article="article" />
       </ContentList>
     </Section>
     <Section>
-      <div>
+      <div class="dark:text-white text-gray-900">
         {{ $t("generic.language.change") }}:
         <button @click="setLocale('en')">
           {{ $t("generic.language.english") }}
