03175649a0afb5f88c866b7c74b0f2df2ca61936
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2024-12-12 00:01:44 +0100

    Make whole blog article clickable

--- components/Blog.vue
+++ components/Blog.vue
@@ -1,22 +1,17 @@
 <template>
   <div
-    :key="article._path"
     class="w-full rounded dark:bg-gray-900 bg-gray-200 dark:text-white text-gray-900 mb-3 sm:mr-3 p-5"
   >
-    <h3 class="leading-loose mb-2 sm:h-40 md:h-20 lg:h-14 xl:h-14">
-      <span class="text-2xl font-bold text-green-500">{{ article.title }}</span>
-      <span class="text-1xl font-normal"> <br />{{ article.date }}</span>
-    </h3>
+    <router-link :key="article._path" :to="article._path">
+      <h3 class="leading-loose mb-2 sm:h-40 md:h-20 lg:h-14 xl:h-14">
+        <span class="text-2xl font-bold text-green-500">{{
+          article.title
+        }}</span>
+        <span class="text-1xl font-normal"> <br />{{ article.date }}</span>
+      </h3>
 
-    <p>{{ article.description }}</p>
-    <p class="mt-6">
-      <a
-        :href="article._path"
-        class="dark:bg-gray-950 bg-white font-bold rounded-full py-4 px-8 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
-      >
-        {{ $t("blogs.readmore") }}
-      </a>
-    </p>
+      <p class="leading-loose">{{ article.description }}</p>
+    </router-link>
   </div>
 </template>
 
