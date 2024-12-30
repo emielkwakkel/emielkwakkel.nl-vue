8477b1f1df641714c33e7a547198ea68ec875b66
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2024-12-30 15:01:55 +0100

    Update blog article styling

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,23 +1,19 @@
 <template>
-  <div class="min-h-screen dark:bg-gray-900 pb-10 bg-gray-200">
+  <div class="min-h-screen dark:bg-gray-900 pb-10 bg-white">
     <ContentDoc v-slot="{ doc }">
-      <header class="px-3 dark:bg-gray-900 bg-white text-gray-200">
-        <div class="flex container mx-auto">
-          <h1
-            class="flex flex-col w-full justify-center text-center md:text-left leading-normal mb-2 text-green-500 text-5xl py-20 font-bold"
-          >
-            {{ doc.title }}
-          </h1>
-        </div>
-      </header>
       <Breadcrumb :links="links"></Breadcrumb>
       <section
-        class="flex container h-full px-3 dark:bg-gray-900 bg-white rounded-xl dark:text-gray-200 text-grey-900 mx-auto my-10 py-10"
+        class="flex container h-full px-3 dark:bg-gray-900 bg-white dark:text-gray-200 text-grey-900 mx-auto py-10"
       >
         <article class="flex flex-col w-full">
           <header>
+            <h1
+              class="flex flex-col w-full leading-normal mb-2 mt-10 text-green-500 text-5xl font-bold"
+            >
+              {{ doc.title }}
+            </h1>
             <p v-if="doc.date">
-              <em>Published on {{ doc.date }}</em>
+              <em class="text-gray-500">Published on {{ doc.date }}</em>
             </p>
           </header>
           <p>
