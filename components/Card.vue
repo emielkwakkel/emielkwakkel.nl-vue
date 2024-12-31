eef6309903b0a420e2bdf926140cc40d6e69d2d5
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2024-12-31 15:25:28 +0100

    Implement generic card component

---
+++ components/Card.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="w-full rounded-lg dark:bg-gray-900 bg-white dark:text-white mb-3 sm:mr-3 p-5 py-10"
+  >
+    <h3 class="leading-loose mb-2 md:h-20 lg:h-14 xl:h-14">
+      <span class="text-2xl font-bold text-green-500">{{ card.title }}</span>
+      <span class="text-gray-500"> <br />{{ card.date }}</span>
+    </h3>
+    <h4 class="sm:pt-3 mb-8">
+      {{ card.title }}
+    </h4>
+    <a
+      v-if="card.link"
+      :href="card.link.href"
+      class="bg-blue-300 text-gray-800 font-bold rounded-full py-4 px-8"
+    >
+      {{ card.link.title }}
+    </a>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Link {
+  href: string;
+  title: string;
+}
+
+interface Card {
+  title: string;
+  date: string;
+  content: string;
+  link?: Link;
+}
+
+defineProps<{
+  card: Card;
+}>();
+</script>
