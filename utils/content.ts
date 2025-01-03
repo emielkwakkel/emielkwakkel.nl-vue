d17da75262e17f97b5aef575406548905ce54b65
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2025-01-03 01:11:00 +0100

    Make addReadmoreToContent also support a single content object

--- utils/content.ts
+++ utils/content.ts
@@ -1,30 +1,16 @@
 export const addReadmoreToContent = (
-  blogs: any[],
+  contents: any,
   t: (key: string) => string,
 ) => {
-  return blogs.map((content) => {
-    return {
-      ...content,
-      link: {
-        title: t("content.read-more"),
-        href: content._path,
-      },
-    };
+  const addLinkToContent = (content: any) => ({
+    ...content,
+    link: {
+      title: t("content.read-more"),
+      href: content._path,
+    },
   });
-};
 
-export const addLinkToEvents = (events: any[], t: (key: string) => string) => {
-  return events.map((content) => {
-    if (!content.website && !content.recording) return content;
-
-    return {
-      ...content,
-      link: {
-        title: content.recording
-          ? t("events.actions.watch-recording")
-          : t("events.actions.event-website"),
-        href: content.recording ? content.recording : content.website,
-      },
-    };
-  });
+  return Array.isArray(contents)
+    ? contents.map(addLinkToContent)
+    : addLinkToContent(contents);
 };
