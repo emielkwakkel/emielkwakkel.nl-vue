f97ab795c28d1662acb63b6328f967fccf9072e4
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2025-01-07 23:27:41 +0100

    Update content styling

--- components/content/ProseH3.vue
+++ components/content/ProseH3.vue
@@ -1,9 +1,15 @@
 <template>
-  <h2 :id="id" class="text-green-500 text-4xl">
+  <h3 :id="id" class="text-blue-300 text-2xl">
     <a :href="`#${id}`"><slot /> </a>
-  </h2>
+  </h3>
 </template>
 
 <script setup lang="ts">
 defineProps<{ id: string }>();
 </script>
+
+<style scoped>
+/* h3 {
+  font-size: 1.5em;
+} */
+</style>
