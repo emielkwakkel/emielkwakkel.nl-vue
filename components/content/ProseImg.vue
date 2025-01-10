6ce694fa22ad8139cea457a1b79b056c21fd450e
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2025-01-10 17:51:58 +0100

    Support floating images in content

--- components/content/ProseImg.vue
+++ components/content/ProseImg.vue
@@ -1,8 +1,26 @@
 <template>
-  <NuxtImg :src="src" :alt="alt" :width="width" :height="height" />
-  <p v-if="alt">
-    <em>{{ alt }}</em>
-  </p>
+  <figure
+    :class="[
+      'my-4',
+      float === 'left' ? 'lg:float-left lg:mr-4' : '',
+      float === 'right' ? 'lg:float-right lg:ml-4' : '',
+      'w-full lg:w-auto', // Full width on mobile, auto width on larger screens
+    ]"
+  >
+    <NuxtImg
+      :src="src"
+      :alt="alt"
+      :width="width"
+      :height="height"
+      class="w-full"
+    />
+    <figcaption
+      v-if="alt"
+      class="my-2 text-sm text-center text-gray-500 dark:text-gray-400"
+    >
+      {{ alt }}
+    </figcaption>
+  </figure>
 </template>
 
 <script setup lang="ts">
@@ -23,12 +41,9 @@ defineProps({
     type: [String, Number],
     default: undefined,
   },
+  float: {
+    type: String,
+    default: "", // Accepts 'left', 'right', or ''
+  },
 });
 </script>
-
-<style scoped>
-p {
-  line-height: 1.8em;
-  font-size: 1.2em;
-}
-</style>
